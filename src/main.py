e405ac065d3829aff0998a874916f9553e3b8ff4
Fawaz Ahmed <fawazahmed0@hotmail.com>
2020-06-15 03:35:44 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ async def queryTraslate(query=''):
     if not query:
         return ''
     try:
-        return translator.translate(query)
+        return translator.translate(query).text
     except:
         raise HTTPException(status_code=404, detail="translate api doesn't work")
 #@app.get("/headers/")
